e1edc374d94fd726718b6efeafd13333b94fafa2
Tomer Faran <tomer.faran@gmail.com>
2024-05-29 22:13:30 +0300

    without path max

--- Lab2/myshell.c
+++ Lab2/myshell.c
@@ -3,40 +3,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h> // For getcwd
-#include <limits.h> // For PATH_MAX
 #include <sys/wait.h>
 #include "LineParser.c"
-
-int main(int argc, char **argv)
-{
-    char input[2048]; /* maximum size of an input*/
-    cmdLine* parseCmd;
-    while(1){ /* infinite loop*/
-        char cwd[_PC_PATH_MAX];
-        if(getcwd(cwd, sizeof(cwd)) != NULL){
-            printf("%s> ", cwd);
-        }
-        else{
-            perror("getcwd() error");
-            return 1;
-        }
-        if(fgets(input, sizeof(input), stdin) == NULL){
-            perror("Error in reading the input");
-            return 1;
-        }
-        parseCmd = parseCmdLines(input);
-        if(parseCmd == NULL){
-            perror("Error in parsing command");
-            continue;
-        }
-        else{
-            execute(&parseCmd);
-            freeCmdLines(parseCmd);
-        }
-    }
-
-    return 0;
-}
+#include <limits.h> // For PATH_MAX
 
 void execute(cmdLine *pCmdLines)
 {
@@ -59,3 +28,36 @@ void execute(cmdLine *pCmdLines)
 
     }
 }
+
+
+int main(int argc, char **argv)
+{
+    char input[2048]; /* Needs to be PATH_MAX*/
+    cmdLine* parseCmd;
+    while(1){ /* infinite loop*/
+        char cwd[2048];
+        if(getcwd(cwd, sizeof(cwd)) != NULL){
+            printf("%s> ", cwd);
+        }
+        else{
+            perror("getcwd() error");
+            return 1;
+        }
+        if(fgets(input, sizeof(input), stdin) == NULL){
+            perror("Error in reading the input");
+            return 1;
+        }
+        parseCmd = parseCmdLines(input);
+        if(parseCmd == NULL){
+            perror("Error in parsing command");
+            continue;
+        }
+        else{
+            execute(parseCmd);
+            freeCmdLines(parseCmd);
+        }
+    }
+
+    return 0;
+}
+
